c89fcd0211933e10a83f44f9999bac8a69e1b8bc
rickFBraga <rickbprabelo@gmail.com>
2024-10-20 23:38:47 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -23,5 +23,5 @@ app.use(errorHandler);
 const port = Number(process.env.PORT) || 5000;
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Servidor rodando na porta ${port}`);
+    console.log(`Servidor rodando na porta ${port}`)
 });
